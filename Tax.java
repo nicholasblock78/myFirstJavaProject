54b2e26ebb379e726c077fe6b46c4c88cadf12d2
Nick Block <nicholasblock78@gmail.com>
2016-12-19 10:44:51 -0600

    Add in hardcoding lists

--- Tax.java
+++ Tax.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 class Tax {
 	String[] exemptItems = {"book", "chocolate", "headache", "pill"};
+	String[] input1 = {"1 book at 12.49", "1 music CD at 14.99","1 chocolate bar at 0.85"};
+	String[] input2 = {"1 imported box of chocolates at 10.00","1 imported box of perfume at 47.50"};
+	String[] input3 = {"1 book at 12.49", "1 bottle of perfume at 18.99", "1 packet of headache pills at 9.75", "1 box of imported chocolates at 11.25"};
 
 	public static void main (String[] args) {
 		Scanner scanner = new Scanner(System.in);
