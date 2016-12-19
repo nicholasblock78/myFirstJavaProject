9e6288680fbfd657f901e5a5252fcbad44d7b2b6
Nick Block <nicholasblock78@gmail.com>
2016-12-19 11:15:42 -0600

    hardcode

--- Receipt.java
+++ Receipt.java
@@ -10,14 +10,3 @@ class Receipt {
 	}
 }
 
- // ArrayList<Integer> weeklyTemperatures = new ArrayList<Integer>();
- // HashMap<String, Integer> myFriends = new HashMap<String, Integer>();
-
- // HashMap<Item obj, Integer(quantity)> receipt = new HashMap
-// let(:input_3) {{
-// 		'imported bottle of perfume' => [1, 27.99],
-// 		'bottle of perfume' => [1, 18.99],
-// 		'packet of headache pills' => [1, 9.75],
-// 		'box of imported chocolates' => [1, 11.25]
-// 	}}
-
